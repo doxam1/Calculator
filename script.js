c0649ea108db9178addf58ff80aa316df12c2cd6
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-10-28 14:57:49 +0300

    math functions working when btn pressed.
    need to add . function.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// basic math functions//
 function add (a, b){
     return a+b;
 };
@@ -14,10 +15,51 @@ function divide (a, b) {
     return a / b;
 };
 
+//some constants for screen, for buttons.
+const screenInput = document.querySelector('.screenInput');
+const screenOutput = document.querySelector('.screenOutput');
 const buttons = document.querySelectorAll('.button');
 
-console.log(buttons)
-// buttons.addEventListener('click', () => {
-//     if (buttons.classList.contains('plus'))
+// flag for reset screen when button pressed after user press equal sign.
+let afterEqual = false;
 
-// })
+// functions for buttons clicks.
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('number')) {
+        if (afterEqual == true) {
+            screenInput.textContent = '';
+            afterEqual = false;
+        }
+        if (screenInput.textContent == '0') screenInput.textContent = '';
+        screenInput.innerHTML += button.textContent
+      }
+      if (button.classList.contains('operator')) {
+        if (screenOutput.textContent != '') screenOutput.textContent = '';
+        screenOutput.innerHTML += screenInput.textContent +' '+ button.textContent;
+        screenInput.textContent ='';
+      }
+      if (button.classList.contains('reset')) {
+        screenInput.textContent = '';
+        screenOutput.textContent = '';
+      }
+      if (button.classList.contains('equal')) {
+        afterEqual = true;
+        screenOutput.innerHTML += ' ' + screenInput.textContent;
+        const toBeCalculated = screenOutput.textContent;
+        const splitToCalculte = toBeCalculated.split(' ');
+        const a = parseInt(splitToCalculte[0]);
+        const b = parseInt(splitToCalculte[2]);
+        const operatorSign = splitToCalculte[1];
+        if (operatorSign == '+') {
+            screenInput.textContent = add (a, b);
+        } else if (operatorSign == '-') {
+            screenInput.textContent = subtract(a, b);
+        } else if (operatorSign == 'x') {
+            screenInput.textContent = multiply(a, b);
+        } else if (operatorSign == '/') {
+            screenInput.textContent = divide(a, b);
+        }
+      }
+    })
+});
