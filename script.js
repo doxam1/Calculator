88863e6a8e1a656b2ae4133e1d09b42540ba977a
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-10-28 13:07:45 +0300

    added almost all the buttons.
    styled a bit with css.
    added some calculator functions.

---
+++ script.js
@@ -0,0 +1,23 @@
+function add (a, b){
+    return a+b;
+};
+
+function subtract (a, b){
+    return a-b;
+};
+
+function multiply (a, b) {
+    return a*b;
+};
+
+function divide (a, b) {
+    return a / b;
+};
+
+const buttons = document.querySelectorAll('.button');
+
+console.log(buttons)
+// buttons.addEventListener('click', () => {
+//     if (buttons.classList.contains('plus'))
+
+// })
